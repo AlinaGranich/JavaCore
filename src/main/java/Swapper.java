51aa09621cd04a40dd66c385c21198931bdf8c06
Alina <alinchik-95@mail.ru>
2021-10-31 00:30:27 +0300

    lesson3 completed

---
+++ src/main/java/Swapper.java
@@ -0,0 +1,7 @@
+public class Swapper<T> {
+    public void swap(T[] array, int first, int second) {
+        T buffer = array[first];
+        array[first] = array[second];
+        array[second] = buffer;
+    }
+}
