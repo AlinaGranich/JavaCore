e39e44e6213c3c0664f95804ab45c5257136cff8
Alina <alinchik-95@mail.ru>
2021-11-14 22:27:34 +0300

    lesson7 completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,8 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String cityId = RequestHandler.getCityId("Saint-Petersburg");
+        System.out.println(RequestHandler.getForecast(cityId));
     }
 }
